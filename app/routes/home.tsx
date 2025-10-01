a4801b3b204d9a76f0ca49025c5884af0303ac4a
Bayu Aji <itsbayua@gmail.com>
2025-10-01 14:04:50 +0700

    Feat: Add resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "constants/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,13 +12,22 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <main className="bg-[url('url('/images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
+
       <section className="main-section">
-        <div className="page-heading">
-          <h1>Track Your Application & Resume Ratings</h1>
-          <h2>Review your submissions and check AI-powered feeback.</h2>
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
